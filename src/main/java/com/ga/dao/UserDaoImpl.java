368f1f2fb8b0af2929e9ffb41e83a25bb46a0be7
Mingjing Tang <tmjcsh@gmail.com>
2019-10-16 01:02:56 -0400

    add User and UserDao and UserService

---
+++ src/main/java/com/ga/dao/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package com.ga.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ga.entity.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+	
+	@Override
+	public List<User> listUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
