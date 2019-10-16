475b4df29b8d0d5b1d4f6e41e27bc907dd743da4
gkopplin <grant.kopplin@gmail.com>
2019-10-15 20:44:09 -0400

    Add required properties to Songs

--- src/main/java/com/ga/entity/Song.java
+++ src/main/java/com/ga/entity/Song.java
@@ -1,5 +1,19 @@
 package com.ga.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Song {
 
+	@Id
+	@Column(name = "song_id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long songId;
+	
+	@Column(nullable = false)
+	private String title;
 }
