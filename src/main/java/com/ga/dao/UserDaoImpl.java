1dd5d1fd90b28d4d7fbb3fddc5814f92589b6e53
Mingjing Tang <tmjcsh@gmail.com>
2019-10-16 23:59:39 -0400

    UserDaoImpl update listUsers method

--- src/main/java/com/ga/dao/UserDaoImpl.java
+++ src/main/java/com/ga/dao/UserDaoImpl.java
@@ -15,101 +15,110 @@ import com.ga.entity.User;
 public class UserDaoImpl implements UserDao {
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	@Override
 	public User signup(User user) {
 //		String roleName = user.getUserRole().getName();
-		
+
 //		UserRole userRole = userRoleDao.getRole(roleName);
 //		
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		try {
 			session.beginTransaction();
-			
+
 //			user.setUserRole(userRole);
-			
+
 			session.save(user);
-			
+
 			session.getTransaction().commit();
 		} finally {
 			session.close();
 		}
-		
+
 		return user;
 	}
 
 	@Override
 	public User login(User user) {
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		User resultUser;
-		
+
 		try {
 			session.beginTransaction();
-			resultUser = (User) session.createQuery("FROM User u WHERE u.username = '" + 
-					user.getUsername() + "' AND u.password = '" + 
-					user.getPassword() + "'").getSingleResult();
+			resultUser = (User) session.createQuery("FROM User u WHERE u.username = '" + user.getUsername()
+					+ "' AND u.password = '" + user.getPassword() + "'").getSingleResult();
 		} finally {
 			session.close();
 		}
-		
+
 		return resultUser;
 	}
 
 	@Override
 	public List<User> listUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> allUsers = null;
+
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			allUsers = session.createQuery("FROM User").getResultList();
+		} finally {
+			session.close();
+		}
+
+		return allUsers;
 	}
-	
+
 	@Override
 	public List<Song> listSongs(Long userId) {
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		List<Song> resultSongs;
-		
+
 		try {
 			session.beginTransaction();
-			
+
 			User fetchedUser = session.get(User.class, userId);
-			
+
 			resultSongs = fetchedUser.getSongs();
 			Hibernate.initialize(resultSongs);
-			
+
 		} finally {
 			session.close();
 		}
-		
+
 		return resultSongs;
 	}
-	
+
 	@Override
 	public List<Song> addSong(Long userId, Long songId) {
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		List<Song> resultSongs = null;
-		
+
 		try {
 			session.beginTransaction();
-			
+
 			User fetchedUser = session.get(User.class, userId);
-			
+
 			if (fetchedUser != null) {
-				
+
 				Song fetchedSong = session.get(Song.class, songId);
-				
+
 				fetchedUser.addSong(fetchedSong);
 				session.getTransaction().commit();
-				
+
 				resultSongs = fetchedUser.getSongs();
 			}
-			
-			
+
 		} finally {
 			session.close();
 		}
-		
+
 		return resultSongs;
 	}
 }
