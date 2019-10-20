465ee7c50cd97fa58fa86cd595b02cdeca9153a5
Mingjing Tang <tmjcsh@gmail.com>
2019-10-20 15:41:04 -0400

    controller hello test sucessful

---
+++ src/test/java/com/ga/controller/UserControllerTest.java
@@ -0,0 +1,83 @@
+package com.ga.controller;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.ga.service.UserService;
+
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	UserController userController;
+	
+	@Before
+	public void init() {
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
+	
+	@Mock
+	private UserService userService;
+	
+	@Test
+	public void helloWorld_HelloWorld_Success() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/user/hello")
+				.accept(MediaType.APPLICATION_JSON);
+		
+		mockMvc.perform(requestBuilder)
+		   .andExpect(status().isOk())
+		   .andExpect(content().string("Hello World!"));
+	}
+	
+	@Test
+	public void signup_User_Success() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("user/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(createUserInJson("joe", "abc", "ROLE_ADMIN"));
+		
+		when(userService.signup(any())).thenReturn("123456");
+		
+		MvcResult result = mockMvc.perform(requestBuilder)
+				   .andExpect(status().isOk())
+				   .andExpect(content().json("{\"token\":\"123456\"}"))
+				   .andReturn();
+
+				System.out.println(result.getResponse().getContentAsString()); 
+	}
+	
+    private static String createUserInJson(String username, String password, String role) {
+        return "{ \"username\": \"" + username + "\", " +
+                "\"password\":\"" + password + "\"," + "{\"userRole\": \"" + createUserRoleInJson(role) + "\"}";
+    }
+    
+    private static String createUserRoleInJson(String role) {
+    	return "\"name\":\"" + role + "\"}";
+    }
+
+	
+	
+	
+
+}
